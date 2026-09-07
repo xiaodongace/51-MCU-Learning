c2bf83cac36b237b5c2fd3144cc1fd1cb2ba7c41
xiaodong <2304334266@qq.com>
2026-09-07 20:22:25 +0800

    修改为定时器1获取时间

--- src/Timer/Timers.c
+++ src/Timer/Timers.c
@@ -7,15 +7,16 @@ volatile u16 system_ms = 0; // 系统时间
 /* 配置Timer1为1 ms自动重装系统节拍。 */
 static void Timer_Config(void) {
     TIM_InitTypeDef TIM_InitStructure; //结构定义
-    //定时器0做16位自动重装, 中断频率为1000HZ
+    /* Timer0 由 RTX51 Tiny 调度器占用；应用层节拍固定使用 Timer1。 */
     TIM_InitStructure.TIM_Mode = TIM_16BitAutoReload;
     //指定工作模式,   TIM_16BitAutoReload,TIM_16Bit,TIM_8BitAutoReload,TIM_16BitAutoReloadNoMask
     TIM_InitStructure.TIM_ClkSource = TIM_CLOCK_1T; //指定时钟源,     TIM_CLOCK_1T,TIM_CLOCK_12T,TIM_CLOCK_Ext
     TIM_InitStructure.TIM_ClkOut = DISABLE; //是否输出高速脉冲, ENABLE或DISABLE
     TIM_InitStructure.TIM_Value = 65536UL - (MAIN_Fosc / 1000UL); //初值,
     TIM_InitStructure.TIM_Run = ENABLE; //是否初始化后启动定时器, ENABLE或DISABLE
-    Timer_Inilize(Timer1, &TIM_InitStructure); //初始化Timer0	  Timer0,Timer1,Timer2,Timer3,Timer4
-    NVIC_Timer0_Init(ENABLE,Priority_0); //中断使能, ENABLE/DISABLE; 优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
+    Timer_Inilize(Timer1, &TIM_InitStructure);
+    /* 开启 Timer1 中断后，Timer1_ISR_Handler 每 1 ms 递增 system_ms。 */
+    NVIC_Timer1_Init(ENABLE,Priority_0);
 }
 
 // 初始化应用层定时器模块。
