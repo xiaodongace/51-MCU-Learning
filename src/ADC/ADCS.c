8ac75e84ebbfab6c887c098645070d72c5f78c3a
xiaodong <2304334266@qq.com>
2026-09-11 10:15:17 +0800

    修改工作频率

--- src/ADC/ADCS.c
+++ src/ADC/ADCS.c
@@ -28,7 +28,7 @@ static void ADC_Config(void) {
     ADC_InitStructure.ADC_SMPduty = 31; //ADC 模拟信号采样时间控制, 0~31（注意： SMPDUTY 一定不能设置小于 10）
     ADC_InitStructure.ADC_CsSetup = 0; //ADC 通道选择时间控制 0(默认),1
     ADC_InitStructure.ADC_CsHold = 1; //ADC 通道选择保持时间控制 0,1(默认),2,3
-    ADC_InitStructure.ADC_Speed = ADC_SPEED_2X1T; //设置 ADC 工作时钟频率	ADC_SPEED_2X1T~ADC_SPEED_2X16T
+    ADC_InitStructure.ADC_Speed = ADC_SPEED_2X8T; //设置 ADC 工作时钟频率	ADC_SPEED_2X1T~ADC_SPEED_2X16T
     ADC_InitStructure.ADC_AdjResult = ADC_RIGHT_JUSTIFIED; //ADC结果调整,	ADC_LEFT_JUSTIFIED,ADC_RIGHT_JUSTIFIED
     ADC_Inilize(&ADC_InitStructure); //初始化
     ADC_PowerControl(ENABLE); //ADC电源开关, ENABLE或DISABLE
