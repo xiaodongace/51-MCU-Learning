815c77a810dd1a4acaf34b9142e4e216e9dcc68e
xiaodong <2304334266@qq.com>
2026-09-13 16:19:51 +0800

    添加串口初始化

--- src/UART/UARTS.c
+++ src/UART/UARTS.c
@@ -4,6 +4,7 @@
 #include    "UARTS.h"
 #include    "Switch.h"
 #include    "Delay.h"
+#include "GPIO.h"
 #include    "LED.h"
 #include    "Motor.h"
 #include    "UartCommand.h"
@@ -15,11 +16,21 @@
  */
 #define UART_MESSAGE_TASK_PERIOD_MS 10
 
+static void Uart_GPIO_Init() {
+    /* ========== Uart1引脚初始化 ========== */
+    P3_MODE_IO_PU(GPIO_Pin_0 | GPIO_Pin_1);
+    /* ========== Uart2引脚初始化 ========== */
+    P1_MODE_IO_PU(GPIO_Pin_0 | GPIO_Pin_1);
+}
+
 
 // 初始化UART通信和串口接收中断。
 void UART_Init(void) {
-    /* 在这里完成 UART 模块的初始化。 */
     COMx_InitDefine COMx_InitStructure;
+
+    Uart_GPIO_Init();
+    /* 在这里完成 UART 模块的初始化。 */
+    /* ========== Uart1 ========== */
     COMx_InitStructure.UART_Mode = UART_8bit_BRTx; //模式, UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
     COMx_InitStructure.UART_BRT_Use = BRT_Timer1; //选择波特率发生器, BRT_Timer1, BRT_Timer2 (注意: 串口2固定使用BRT_Timer2)
     COMx_InitStructure.UART_BaudRate = 115200ul; //波特率, 一般 110 ~ 115200
@@ -27,8 +38,18 @@ void UART_Init(void) {
     COMx_InitStructure.BaudRateDouble = DISABLE; //波特率加倍, ENABLE或DISABLE
     UART_Configuration(UART1, &COMx_InitStructure); //初始化串口1 UART1,UART2,UART3,UART4
     NVIC_UART1_Init(ENABLE,Priority_1); //中断使能, ENABLE/DISABLE; 优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
-
     UART1_SW(UART1_SW_P30_P31); //UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17,UART1_SW_P43_P44
+
+    // ================UART2  P10 P11================
+    COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;	//模式, UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+    COMx_InitStructure.UART_BRT_Use   = BRT_Timer2;			//选择波特率发生器, BRT_Timer1, BRT_Timer2 (注意: 串口2固定使用BRT_Timer2)
+    COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率, 一般 110 ~ 115200
+    COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+    COMx_InitStructure.BaudRateDouble = DISABLE;			//波特率加倍, ENABLE或DISABLE
+    UART_Configuration(UART2, &COMx_InitStructure);		//初始化串口1 UART1,UART2,UART3,UART4
+
+    NVIC_UART2_Init(ENABLE,Priority_1);		//中断使能, ENABLE/DISABLE; 优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
+    UART2_SW(UART2_SW_P10_P11);		// 引脚选择
 }
 
 
